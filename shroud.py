90ce76c2c7b234570744c1a595a586cf74eb6b59
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-08-05 09:11:42 +0200

    Support multiple search directories, close #3
    * add new Context class
    * rename require(path) parameter to "file"
    * introduce new "path" parameter that should be a list of
      directories, this directory will be searched for candidate
      files to load
    * add _get_best_candidate()
    * add bcsuffix global

--- shroud.py
+++ shroud.py
@@ -35,10 +35,14 @@ __author__ = 'Niklas Rosenstein <rosensteinniklas(at)gmail.com>'
 __version__ = '0.3'
 
 import errno
+import itertools
 import marshal
 import os
 import types
 import sys
+
+#: The suffix that is appended to bytecode files.
+bcsuffix = 'c@' + sys.version[:3]
 
 #: This dictionary maps absolute filenames to the Python module
 #: that are loaded by :func:`require`.
@@ -52,7 +56,32 @@ class RequireError(ImportError):
   """
 
 
-def require(path, directory=None, reload=False, cascade=False, inplace=False):
+class Context(object):
+  """
+  Container for contextual information for a module import.
+
+  .. attribute:: path
+
+    List of additional search directories.
+
+  .. attribute:: cascade_reload
+
+    True if a cascade reload was issued when the module was (re-)loaded.
+    This will be automatically disabled after the module was finally
+    imported.
+
+  .. attribute:: reload_inplace
+
+    True if the current reload procedura should happen inplace.
+  """
+
+  def __init__(self, path, reload=False, cascade=False, inplace=False):
+    self.path = list(path)
+    self.cascade_reload = bool(reload and cascade)
+    self.reload_inplace = bool(reload and inplace)
+
+
+def require(file, directory=None, path=(), reload=False, cascade=False, inplace=False):
   """
   Loads a Python module by filename. Can fall back to bytecode cache file
   if available and writes them if ``sys.dont_write_bytecode`` is not enabled.
@@ -60,11 +89,14 @@ def require(path, directory=None, reload=False, cascade=False, inplace=False):
   will be the path to the Python source file (even for cache files and even
   if the source file does not exist).
 
-  :param path: The path to the Python module. If it contains the ``.py``,
+  :param file: The path to the Python module. If it contains the ``.py``,
     suffix the Python module must but a file, otherwise it can also be a
     directory of which the ``__init__.py`` file is loaded.
-  :param directory: The directory to consider *path* relative to. If omitted,
+  :param directory: The directory to consider *file* relative to. If omitted,
     it will be read from the calling stackframe's globals.
+  :param path: A list of secondary search directories to look out for the
+    Python module required with the *file* parameter. Subsequent calls from
+    inside the module will inherit this parameter.
   :param reload: If this parameter is True, the module will be reloaded if
     it was already loaded.
   :param cascade: If this parameter and *reload* is True, the reload will
@@ -77,12 +109,16 @@ def require(path, directory=None, reload=False, cascade=False, inplace=False):
   :return: :class:`types.ModuleType`
   """
 
+  ofile = file
   frame = sys._getframe(1).f_globals
-  parent_info = frame.get('__shroud__', None)
-  if isinstance(parent_info, dict) and parent_info.get('cascade-reload'):
+  context = frame.get('__shroud__', None)
+  if isinstance(context, Context) and context.cascade_reload:
     reload = True
     cascade = True
-    inplace = bool(parent_info.get('reload-inplace'))
+    inplace = cascade.reload_inplace
+  if isinstance(context, Context):
+    path = list(path)
+    path.extend(context.path)
 
   if not directory:
     # Determine the directory to load the module from the callers
@@ -92,60 +128,41 @@ def require(path, directory=None, reload=False, cascade=False, inplace=False):
       raise RuntimeError('require() caller must provide __file__ variable')
     directory = os.path.abspath(os.path.dirname(caller_file))
 
-  if not os.path.isabs(path):
-    path = os.path.join(directory, path)
-  path = os.path.normpath(os.path.abspath(path))
-
-  # Automatically append the .py suffix or load __init__.py if the path
-  # points to a directory.
-  if os.path.isfile(path):
-    pass  # use as-us
-  elif os.path.isdir(path):
-    path = os.path.join(path, '__init__.py')
-  elif not path.endswith('.py'):
-    path += '.py'
+  file = _get_best_candidate(file, directory, path)
+  if not file:
+    raise RequireError(ofile)
 
   # Check if we already loaded this module and return it preemptively.
-  mod = modules.get(path)
+  mod = modules.get(file)
   if mod and not reload:
     return mod
 
   # The filename and file type that we're ultimately going to load
   # depends on the availability of the cache.
-  filename = path
+  fullname = file
   mode = 'source'
-
-  # Determine the cache path and if either of the source or cache
-  # files exist and which we should load.
-  bc_path = path + 'c@' + sys.version[:3]
-  bc_mod = _getmtime_or_none(bc_path)
-  path_mod = _getmtime_or_none(path)
-  if path_mod is None and bc_mod is None:
-    raise RequireError(path)  # Neither of the two files exist
-  elif path_mod is not None and bc_mod is not None:
-    if path_mod <= bc_mod:
-      # We can load the cache since the source hasn't been touched since.
-      filename = bc_path
-      mode = 'bytecode'
-  elif path_mod is None:
-    filename = bc_path
+  if file.endswith(bcsuffix):
+    file = file[:-len(bcsuffix)]
     mode = 'bytecode'
+
+  # Create the module context information.
+  context = Context(path, reload, cascade, inplace)
 
   # Create and initialize the new module.
   if not (mod and reload and inplace):
-    mod = types.ModuleType(path)
-  mod.__file__ = filename
-  mod.__shroud__ = {'cascade-reload': bool(reload and cascade), 'reload-inplace': inplace}
+    mod = types.ModuleType(file)
+  mod.__file__ = fullname
+  mod.__shroud__ = context
   mod.require = require
-  modules[path] = mod
+  modules[file] = mod
 
   # Load and execute the module code.
   try:
     if mode == 'source':
-      with open(filename, 'r') as fp:
-        code = compile(_preprocess_source(fp.read()), filename, 'exec')
+      with open(fullname, 'r') as fp:
+        code = compile(_preprocess_source(fp.read()), fullname, 'exec')
     elif mode == 'bytecode':
-      with open(filename, 'rb') as fp:
+      with open(fullname, 'rb') as fp:
         code = marshal.load(fp)
     else:
       assert False
@@ -154,21 +171,55 @@ def require(path, directory=None, reload=False, cascade=False, inplace=False):
     # If anything bad happened, remove the module from the global
     # module cache again. Just be nice and tolerant and allow the
     # module to have been removed already.
-    modules.pop(path, None)
+    modules.pop(file, None)
     raise
 
   # Write the bytecode cache, but don't be cocky if it doesn't work.
   if not sys.dont_write_bytecode:
-    cache_dirname = os.path.dirname(bc_path)
+    cache_dirname = os.path.dirname(file)
     try:
       if not os.path.isdir(cache_dirname):
         os.makedirs(cache_dirname)
-      with open(bc_path, 'wb') as fp:
+      with open(file + bcsuffix, 'wb') as fp:
         marshal.dump(code, fp)
     except (OSError, IOError):
       pass
 
   return mod
+
+
+def _get_best_candidate(file, main_dir, path):
+  """
+  Finds the best candidate that we can load for the specified *file*
+  parameter. The returned string can contain or not contain the bytecode
+  suffix :data:`bcsuffix`.
+  """
+
+  for dirname in itertools.chain([main_dir], path):
+    curr = file
+    if not os.path.isabs(curr):
+      curr = os.path.abspath(os.path.join(dirname, curr))
+
+
+    if os.path.isfile(curr):
+      return curr
+    elif os.path.isfile(curr + bcsuffix):
+      return curr + bcsuffix  # use existing cache file
+    elif os.path.isdir(curr):
+      temp = os.path.join(curr, '__init__.py')
+      if not os.path.isfile(temp):
+        temp += bcsuffix  # check existing cache file
+      if os.path.isfile(temp):
+        return temp
+
+    if os.path.isabs(file):
+      break  # We don't need to do this again if the file is absolute
+
+  if not file.endswith('.py'):
+    # Try again with .py suffix.
+    return _get_best_candidate(file + '.py', main_dir, path)
+
+  return None
 
 
 def _preprocess_source(source):
