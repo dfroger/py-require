faeb639cdd2d479cc996bd3f3d7f8f026b8415e4
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-08-06 18:40:24 +0200

    Prepare 0.10 release

--- require.py
+++ require.py
@@ -33,7 +33,7 @@ easily result in dependency conflicts.
 """
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas(at)gmail.com>'
-__version__ = '0.9'
+__version__ = '0.10'
 
 import errno
 import functools
