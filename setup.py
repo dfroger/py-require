d781cd833753bb2e5674257fb6d1fbac57876b8e
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-10-08 11:53:52 +0200

    Prepare 0.17 release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def restify():
 
 setup(
   name = "py-require",
-  version = "0.16",
+  version = "0.17",
   description = "require() for Python",
   long_description = restify(),
   author = "Niklas Rosenstein",
